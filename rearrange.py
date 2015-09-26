5ef6cdfe24fed4403ee16e8dd9bf7bd97a063f99
Ignat Remizov <ignat980@gmail.com>
2015-09-25 18:30:53 -0700

    Added rearrange script
    the script takes a bunch of arguments and shuffles them

---
+++ rearrange.py
@@ -0,0 +1,15 @@
+import random
+import sys
+
+
+# based on random's shuffle
+def ig_shuffle(x):
+    for i in reversed(range(1, len(x))):
+        j = random.randrange(0, i+1)
+        x[i], x[j] = x[j], x[i]
+    return x
+
+
+# This will print the arguments in a random order
+if __name__ == '__main__':
+    print(ig_shuffle(sys.argv[1:]))
