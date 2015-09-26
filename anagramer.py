f1a2f7a6f5cda896c286395784b5854664733163
Ignat Remizov <ignat980@gmail.com>
2015-09-26 03:09:28 -0700

    started virtualenv + made anagram only show normal words

--- anagramer.py
+++ anagramer.py
@@ -1,7 +1,20 @@
 import sys
-import itertools
+import enchant  # Uses PyEnchant for connecting to enchant
+
+
+def anagrams(word):
+    """ Generate all of the permutations of a word. """
+    if len(word) < 2:  # A letter does not need to be permutated
+        yield word
+    else:
+        for i, letter in enumerate(word):
+            if letter not in word[:i]:  # Avoid duplicates
+                for j in anagrams(word[:i]+word[i+1:]):  # Yay recursion
+                    yield j+letter
 
 
 if __name__ == "__main__":
-    for i in itertools.permutations(sys.argv[1]):
-        print(''.join(i))
+    d = enchant.Dict('en_US')  # Initialize the dictionary
+    for i in anagrams(sys.argv[1]):
+        if d.check(i):
+            print(i)
