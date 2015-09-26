2f34d26ce457b521652f258ae67e25f4450dd2be
Ignat Remizov <ignat980@gmail.com>
2015-09-26 04:05:07 -0700

    improved anagrammer: prints all words, including words not using all letters

--- anagramer.py
+++ anagramer.py
@@ -1,20 +1,16 @@
 import sys
-import enchant  # Uses PyEnchant for connecting to enchant
+import itertools
+import enchant
 
 
-def anagrams(word):
-    """ Generate all of the permutations of a word. """
-    if len(word) < 2:  # A letter does not need to be permutated
-        yield word
-    else:
-        for i, letter in enumerate(word):
-            if letter not in word[:i]:  # Avoid duplicates
-                for j in anagrams(word[:i]+word[i+1:]):  # Yay recursion
-                    yield j+letter
-
+anagrams = []
 
 if __name__ == "__main__":
-    d = enchant.Dict('en_US')  # Initialize the dictionary
-    for i in anagrams(sys.argv[1]):
-        if d.check(i):
-            print(i)
+    d = enchant.Dict('en_US')
+    for i in range(2, len(sys.argv[1])):
+        for word in itertools.permutations(sys.argv[1], i):
+            r = ''.join(word)
+            if d.check(r):
+                if r not in anagrams:
+                    anagrams.append(r)
+                    print(r)
