682523bb50434eadf8596901cdc12411f314ce83
Ignat Remizov <ignat980@gmail.com>
2015-09-27 02:53:43 -0700

    Better organization, updated memory issue with random dictionary
    it now auto-closes the opened file

--- randomDictionaryWords.py
+++ randomDictionaryWords.py
@@ -1,7 +1,8 @@
 import sys
 import random
 
-dictionary = open('/usr/share/dict/words').read().split('\n')
+with open('/usr/share/dict/words') as words:
+    dictionary = words.read().split('\n')
 
 if __name__ == '__main__':
     print((' '.join([random.choice(dictionary) for i in range(int(sys.argv[1]))]) + ".").capitalize())
