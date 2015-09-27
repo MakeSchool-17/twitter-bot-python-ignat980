e73b9b0ee649f95040475a00c4bc3421d15fda4c
Ignat Remizov <ignat980@gmail.com>
2015-09-27 03:01:40 -0700

    commented pythonic magic, also more readable code

--- randomDictionaryWords.py
+++ randomDictionaryWords.py
@@ -1,8 +1,11 @@
 import sys
 import random
 
-with open('/usr/share/dict/words') as words:
-    dictionary = words.read().split('\n')
+with open('/usr/share/dict/words') as wordsFile:
+    dictionary = wordsFile.read().split('\n')
 
 if __name__ == '__main__':
+    # Prints a string that has the first word capitalized
+    # and a . at the end, and it comes from a generator which chooses
+    # a random word from a dictionary for the int supplied to the script
     print((' '.join([random.choice(dictionary) for i in range(int(sys.argv[1]))]) + ".").capitalize())
